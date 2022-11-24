fec19593d5cc21ff9a192bbfe20f6437a05a7b90
priyacodmi <priyaranjan@codmitechnologies.com>
2022-11-24 13:20:17 +0530

    button is created

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import { Button } from './components/CustomButton';
 
 function App() {
   return (
     <div className="App">
       Hello World!
+      <Button>CustomButton</Button>
     </div>
   );
 }
---
+++ app/src/components/CustomButton.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Button=({props})=>{
+  return(
+    <>
+      <button type={props}></button>
+    </>
+  )
+}
+
+
+
+import React from "react";
+
+export const Button=()=>{
+  return(
+    <>
+      <button></button>
+    </>
+  )
+}
