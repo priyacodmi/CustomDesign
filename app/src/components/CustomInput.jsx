b75a3c77ab9d34a6678b0027beea079706ecaab7
priyacodmi <priyaranjan@codmitechnologies.com>
2022-11-24 18:37:16 +0530

    validation is done in Sign-Up page

--- app/src/components/CustomInput.jsx
+++ app/src/components/CustomInput.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import './Styles/input.css';
 
-export const Input = ({ type,name,value,onChange, placeholder }) => {
+export const Input = ({ type,name,value,onChange,onKeyUp,onBlur, placeholder }) => {
   return (
 
     <div>
-      <input className="input" type={type} name={name} onChange={onChange} value={value} placeholder={placeholder} />
+      <input className="input" type={type} name={name} onChange={onChange} onKeyUp={onKeyUp} onBlur={onBlur} value={value} placeholder={placeholder} />
     </div>
 
 
--- app/src/components/SignUp.jsx
+++ app/src/components/SignUp.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "./CustomInput";
 import { Button } from "./CustomButton";
 import { Form } from "./CustomForm";
 import { FormLabel } from "./CustomLabel";
-import { useState } from "react";
+
 
 export const SignUp = () => {
   const initialState={
     name:"",
     email:"",
-    password:""
+    password:"",
+    confirmPassword:""
   }
   const [user,setUser]=useState(initialState);
+  const [nameErr,setNameErr]=useState("");
+  const [emailErr,setEmailErr]=useState("");
+  const [passErr,setPassErr]=useState("");
+  const [conPassErr,setConPassErr]=useState("");
 
   const handleChange=(e)=>{
     const {value,name}=e.target;
@@ -21,22 +26,106 @@ export const SignUp = () => {
     });
   }
 
+  const { name, email, password, confirmPassword}= user;
+
+
+  // name validation
+  const nameValidation=()=>{
+    const nameRegex="[a-zA-Z][a-zA-Z ]+[a-zA-Z]$";
+    if(!name){
+      setNameErr("User name is required!");
+    }
+    else if(name.length<8 || name.length>20){
+      setNameErr("User name must be 8 to 20 characters long!");
+    }
+    else if(!name.match(nameRegex)){
+      setNameErr("Please enter a valid name, name should contains only alphabate characters!");
+    }
+    else{
+      setNameErr("");
+    }
+    return true;
+  }
+
+    
+    //email validation
+  const emailValidation=()=>{
+    const emailRegex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+    if(!email){
+      setEmailErr("Email is required!");
+    }
+    else if(!email.match(emailRegex)){
+      setEmailErr("Please enter a valid email!");
+    }
+    else{
+      setEmailErr("");
+    }
+    return true;
+  }
+
+
+    //password validation
+  const passValidation=()=>{
+    const passRegex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$";
+    if(!password){
+      setPassErr("Please fill your password!");
+    }
+    else if(!user.password.match(passRegex)){
+      setPassErr("Password must contain one specail character, one lowercse character, one upercase character, and it should be of 8-10 characters long!");
+    }
+    else{
+      setPassErr("");
+    }
+    return true;
+  }
+    
+
+
+    //confirm password validation
+  const confPassValidation=()=>{
+    const confPassRegex=password;
+    if(!confirmPassword){
+      setConPassErr("Please confirm your password!");
+    }
+    else if(!confirmPassword.match(confPassRegex)){
+      setConPassErr("Password did not match!");
+    }
+    else{
+      setConPassErr("");
+    }
+    
+    return true;
+  }
+
+  
   const handleSubmit=(e)=>{
     e.preventDefault();
-    console.log("user",user);
-    setUser(initialState);
+    nameValidation();
+    emailValidation();
+    passValidation();
+    confPassValidation();
+    if(name!=="" && email!=="" && password!=="" && confirmPassword!==""){
+      console.log("user",user);
+    }
   }
+
 
   return (
     <>
       <h3>Welcome To SignUp Page &#x1F60D;</h3>
       <Form onSubmit={handleSubmit}>
         <FormLabel>User Name :</FormLabel>
-        <Input type="text" name="name" value={user.name} onChange={handleChange} placeholder="Enter your name...*" />
+        <Input type="text" name="name" value={user.name} onChange={handleChange} onKeyUp={nameValidation} onBlur={nameValidation} placeholder="Enter your name...*" />
+        {nameErr && <p style={{color:"red"}}>{nameErr}</p>}
         <FormLabel>Email :</FormLabel>
-        <Input type="email" name="email" value={user.email} onChange={handleChange} placeholder="Enter your email...*" />
+        <Input type="email" name="email" value={user.email} onChange={handleChange} onKeyUp={emailValidation} onBlur={emailValidation} placeholder="Enter your email...*" />
+        {emailErr && <p style={{color:"red"}}>{emailErr}</p>}
         <FormLabel>Password :</FormLabel>
-        <Input type="password" name="password" value={user.password} onChange={handleChange} placeholder="Enter your password...*" />
+        <Input type="password" name="password" value={user.password} onChange={handleChange} onKeyUp={passValidation} onBlur={passValidation} placeholder="Enter your password...*" />
+        {passErr && <p style={{color:"red"}}>{passErr}</p>}
+        <FormLabel>Confirm Password :</FormLabel>
+        <Input type="password" name="password" value={user.confirmPassword} onChange={handleChange} onKeyUp={confPassValidation} onBlur={confPassValidation} placeholder="Confirm your password...*" />
+        {conPassErr && <p style={{color:"red"}}>{conPassErr}</p>}
         <Button>Sign Up</Button>
       </Form>
     </>
