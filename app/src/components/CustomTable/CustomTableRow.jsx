5573b34411a95c21448b2192a353240e38454cf9
priyacodmi <priyaranjan@codmitechnologies.com>
2022-12-05 13:52:15 +0530

    css of table is updated

--- app/src/components/CustomTable/CustomTableRow.jsx
+++ app/src/components/CustomTable/CustomTableRow.jsx
@@ -6,7 +6,7 @@ export const CustomTableRow = ({ rowData }) => {
   return (
     <tr className="tableRow">
       {rowData.map((col,index)=>(
-        <td key={index}>{col.value}</td>
+        <td className="tableData" key={index}>{col.value}</td>
       ))}  
     </tr>
   )
