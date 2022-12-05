86272f0935bcf343930244cb3bd4a078d7c0fbae
priyacodmi <priyaranjan@codmitechnologies.com>
2022-12-05 12:57:08 +0530

    delete and update icons added

--- app/src/AdminDashboard/Dashboard.jsx
+++ app/src/AdminDashboard/Dashboard.jsx
@@ -9,19 +9,19 @@ import "./dashboard.css";
 export const Dashboard=()=>{
     const options=[{value:"Grade A"},{value:"Grade B"},{value:"Grade C"}]
     const headerData=[[{value:<Input type="checkbox"/>},{value:"Roll_Number"},{value:"Student_Name"}, {value:"Course"}, {value:"Marks"}, 
-    {value:"Remove"}, {value:"Update"},{value:"Dropdown"}
+    {value:"Remove"}, {value:"Update"},{value:"Grade"}
 ]];
   const tableData=[[{value:<Input type="checkbox"/>},{value:101},{value:"Ajay Kumar"},{value:"Btech"},{value:"86%"},
-   {value:<Button>Remove</Button>},{value:<Button>Update</Button>},{value:<DropDown name="dropdown" options={options}/>}
+   {value:<Button>❌</Button>},{value:<Button>✏️</Button>},{value:<DropDown name="dropdown" options={options}/>}
 ],
 [{value:<Input type="checkbox"/>},{value:102},{value:"Ramesh Kumar"},{value:"BCA"},{value:"80%"},
-   {value:<Button>Remove</Button>},{value:<Button>Update</Button>},{value:<DropDown name="dropdown" options={options}/>}
+   {value:<Button>❌</Button>},{value:<Button>✏️</Button>},{value:<DropDown name="dropdown" options={options}/>}
 ],
 [{value:<Input type="checkbox"/>},{value:103},{value:"Vinay Kumar"},{value:"Mtech"},{value:"79%"},
-   {value:<Button>Remove</Button>},{value:<Button>Update</Button>},{value:<DropDown name="dropdown" options={options}/>}
+   {value:<Button>❌</Button>},{value:<Button>✏️</Button>},{value:<DropDown name="dropdown" options={options}/>}
 ],
 [{value:<Input type="checkbox"/>},{value:104},{value:"Vikash Kumar"},{value:"MCA"},{value:"76%"},
-   {value:<Button>Remove</Button>},{value:<Button>Update</Button>},{value:<DropDown name="dropdown" options={options}/>}
+   {value:<Button>❌</Button>},{value:<Button>✏️</Button>},{value:<DropDown name="dropdown" options={options}/>}
 ]]
   return(
     <div className="tableContainer">
